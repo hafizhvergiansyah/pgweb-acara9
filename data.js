8c65000063f074b9bc06ea38e53d804398a64515
hafizhver <piagembul5555@gmail.com>
2023-11-12 19:36:26 +0700

    fix legend and clusters

---
+++ data.js
@@ -0,0 +1,805 @@
+const data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.1",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.23462955,
+          -7.72581681,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.2",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.23463349,
+          -7.7268018,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.3",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.23040126,
+          -7.74253129,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.4",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.24650247,
+          -7.80001521,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.5",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.23300071,
+          -7.76631884,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.6",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.47070616,
+          -7.7560841,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.7",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.49019919,
+          -7.75624014,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.8",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.39623061,
+          -7.75722127,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.9",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.45557225,
+          -7.76279252,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.10",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.48857428,
+          -7.76566014,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.11",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.46756469,
+          -7.77081277,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.12",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.39135909,
+          -7.77706646,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.13",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.43018995,
+          -7.78283042,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.14",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.49348045,
+          -7.78319845,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.15",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.48233452,
+          -7.79140282,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.16",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.45839877,
+          -7.79227164,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.17",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.44260581,
+          -7.80548242,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.18",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.44301714,
+          -7.80786828,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.19",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.4850288,
+          -7.81106349,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.20",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.46942455,
+          -7.8148518,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.21",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.43075412,
+          -7.82088129,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.22",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.29420083,
+          -7.7583873,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.23",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.25900663,
+          -7.76062135,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.24",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.34474783,
+          -7.76143384,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.25",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.26658191,
+          -7.76189697,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.26",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.32051668,
+          -7.76469645,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.27",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.28897934,
+          -7.76664626,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.28",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.36212087,
+          -7.7673241,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.29",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.29310575,
+          -7.76818509,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.30",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.29396676,
+          -7.76822804,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.31",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.25352155,
+          -7.77324641,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.32",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.3341128,
+          -7.77484065,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.33",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.32553068,
+          -7.7755583,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.34",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.30407618,
+          -7.77703085,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.35",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.29046006,
+          -7.77907807,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.36",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.27056809,
+          -7.78521788,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.37",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.33416179,
+          -7.78805012,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.38",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.32172484,
+          -7.79572239,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.39",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.3202163,
+          -7.80051763,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.40",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.30389156,
+          -7.80393136,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.41",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.34340491,
+          -7.63310539,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.42",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.36919778,
+          -7.6522473,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.43",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.32582354,
+          -7.65230858,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.44",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.32661575,
+          -7.65266104,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Camat"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.45",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.35633688,
+          -7.65309943,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.46",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.33005056,
+          -7.66315563,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.47",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.30716656,
+          -7.66449083,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.48",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.31763241,
+          -7.67074195,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.49",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.30547794,
+          -7.67492736,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "FAS_PERKANTORAN_PT_SLEMAN_Proj.50",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.35768853,
+          -7.68168465,
+          0
+        ]
+      },
+      "geometry_name": "geom",
+      "properties": {
+        "remark": "Kantor Kepala Desa"
+      }
+    }
+  ],
+}
